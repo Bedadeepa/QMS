27542ef54f72986139d2c776d98536d12dbef88e
Bedadeepa <sahoodeepa97@gmail.com>
2022-03-19 15:44:48 +0530

    First Commit

---
+++ Domain/AppCtx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Data.Entity.Infrastructure;
+//using GenericwithUnitofwork.Models;
+
+namespace Domain
+{
+    public class AppCtx : DbContext
+    {
+        public AppCtx() : base("name=DefaultConnection")
+        {
+        }
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<TokenRegistration> TokenRegistrations { get; set; }
+        public DbSet<DepartmentMaster> DepartmentMasters { get; set; }
+        public DbSet<CounterMaster> CounterMasters { get; set; }
+        
+        public static AppCtx Create()
+        {
+            return new AppCtx();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
